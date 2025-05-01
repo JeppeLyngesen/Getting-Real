36dab042c8c1b733da884794185bb33255682861
Jeppe Lyngesen <jeppe_lyngesen@hotmail.com>
2025-05-01 09:23:57 +0200

    Klasser lavet

--- Getting Real/Car.cs
+++ Getting Real/Car.cs
@@ -8,5 +8,54 @@ namespace Getting_Real
 {
     internal class Car
     {
+        private int _currentId { get; set; }
+        private int _newId { get; set; }
+        public enum CarType
+        {
+            Sedan = 0,
+            StationCar = 1,
+            SUV = 2,
+            Minivan = 3
+        }
+        public CarType Type { get; set; }
+
+        public Car(int currentId, int newId, CarType type)
+        {
+            this._currentId = currentId;
+            this._newId = newId;
+            this.Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"{_currentId};{_newId};{Type}";
+        }
+
+        public static Car FromString(string input)
+        {
+            var parts = input.Split(';');
+
+            if (parts.Length != 3)
+            {
+                throw new FormatException("Input needs exactly two semicolon seperators");
+            }
+
+            if (!int.TryParse(parts[0], out int currentId))
+            {
+                throw new FormatException("currentId is not an integer");
+            }
+
+            if (!int.TryParse(parts[1], out int newId))
+            {
+                throw new FormatException("newId is not an integer");
+            }
+
+            if (!Enum.TryParse(parts[2], out CarType type))
+            {
+                throw new FormatException("CarType is not a vaild CarType");
+            }
+
+            return new Car(currentId, newId, type);
+        }
     }
 }
--- Getting Real/Company.cs
+++ Getting Real/Company.cs
@@ -8,5 +8,39 @@ namespace Getting_Real
 {
     internal class Company
     {
+        private string _name { get; set; }
+        private string _email { get; set; }
+        private int _phoneNumber { get; set; }
+        public Company(string name, string email, int phoneNumber)
+        {
+            this._name = name;
+            this._email = email;
+            this._phoneNumber = phoneNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name};{_email};{_phoneNumber}";
+        }
+
+        public static Company FromString(string input)
+        {
+            var parts = input.Split(";");
+
+            if (parts.Length != 3)
+            {
+                throw new FormatException("Input needs exactly one semicolon seperator");
+            }
+
+            string name = parts[0];
+            string email = parts[1];
+
+            if (!int.TryParse(parts[2], out int phoneNumber))
+            {
+                throw new FormatException("phoneNumber is not an integer");
+            }
+
+            return new Company(name, email, phoneNumber);
+        }
     }
 }
