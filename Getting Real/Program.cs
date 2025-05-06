afb1cc1e89f5c24a65dc079e99fa6711a4c1e1fa
CeciliaHafezan <ceciliaabel@nalaboeger.dk>
2025-05-06 14:45:12 +0200

    ASCII logo

--- Getting Real/Program.cs
+++ Getting Real/Program.cs
@@ -3,11 +3,32 @@
 namespace Getting_Real
 {
     internal class Program
+     
     {
+        private static ASCII logo = new ASCII();
         static void Main(string[] args)
         {
+
+            Thread logoThread = new Thread(ShowLogo);
+            logoThread.Start();
+
+
+            Thread.Sleep(3000);
+
+
             ManagementSystem Sydtrafik = new ManagementSystem();
             Sydtrafik.Start();
+
+
+
         }
+
+        static void ShowLogo()
+        {
+            Console.Clear();
+
+            logo.ShowSydtrafikLogo();
+        }
+
     }
 }
